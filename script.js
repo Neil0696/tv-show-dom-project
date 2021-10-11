c7fd6fb8a3a5d3cd0c5a65f3c75488f8129aae57
O'neil Malik <oneilmalik09@gmail.com>
2021-10-11 20:27:36 +0100

    level 300 done

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //creating a global variable
 const allEpisodes = getAllEpisodes();
 
@@ -9,22 +8,22 @@ function setup() {
 window.onload = setup;
 
 function makePageForEpisodes(episodeList) {
-    const rootElem = document.getElementById("root");
-    rootElem.textContent = "";
- 
-    episodeList.forEach(e => {
-    const divEl = document.createElement('div'); 
+  const rootElem = document.getElementById("root");
+  rootElem.textContent = "";
+
+  episodeList.forEach(e => {
+    const divEl = document.createElement("div");
     divEl.className = "divEl";
-    const titleEl = document.createElement('h2');
+    const titleEl = document.createElement("h2");
     titleEl.id = "titleEl";
-    const paraEl = document.createElement('p');
+    const paraEl = document.createElement("p");
     paraEl.id = "summaryEl";
-    const imgEl = document.createElement('img');
+    const imgEl = document.createElement("img");
     imgEl.id = "imageEl";
     const aTag = document.createElement("a");
     aTag.id = "aTag";
     // const linkEl = document.createTextNode("Original data from TVMaze.com"); //adds text inside the element
-  
+
     rootElem.appendChild(divEl);
     divEl.appendChild(titleEl);
     divEl.appendChild(imgEl);
@@ -32,57 +31,59 @@ function makePageForEpisodes(episodeList) {
     divEl.appendChild(aTag);
     // aTag.appendChild(linkEl);
 
-    titleEl.innerHTML = `${e.name} - S${(e.season.toString().padStart(2, '0'))}E${(e.number.toString().padStart(2, '0'))}`;
+    titleEl.innerHTML = `${e.name} - S${e.season
+      .toString()
+      .padStart(2, "0")}E${e.number.toString().padStart(2, "0")}`;
     imgEl.src = e.image.original;
     paraEl.innerHTML = e.summary;
     // aTag.title = 'this is a link';
-    // aTag.href = e._links.self.href; //shows the api link data object from episode.js file 
+    // aTag.href = e._links.self.href; //shows the api link data object from episode.js file
     aTag.innerHTML = `<a href= ${e.url} target="_blank">Episode information</a>`;
+
+    //Select menu option els created & appended for each episode
+    let optionEl = document.createElement("option");
+    selectEl.appendChild(optionEl);
+    optionEl.innerText = `${e.name} - S${e.season
+      .toString()
+      .padStart(2, "0")}E${e.number.toString().padStart(2, "0")}`;
   });
 }
 
 //Level 200 (search bar)
 
-const searchBtnEl = document.getElementById('search-btn');
-const numberDisplayEl = document.getElementById('number-display');
+const searchBtnEl = document.getElementById("search-btn");
+const numberDisplayEl = document.getElementById("number-display");
 
-
-searchBtnEl.addEventListener('input', (e) => {
-  
+searchBtnEl.addEventListener("input", e => {
   let search = e.target.value.toLowerCase();
-  
-  let filteredEps = allEpisodes.filter((episode) => {
+
+  let filteredEps = allEpisodes.filter(episode => {
     return (
       episode.name.toLowerCase().includes(search) ||
       episode.summary.toLowerCase().includes(search)
-      );
-    });
-
-    numberDisplayEl.innerText = `Displaying ${filteredEps.length}/${allEpisodes.length} Episodes`;
-
-    makePageForEpisodes(filteredEps);
-
+    );
   });
 
-  //Level 300 (Select input)
+  numberDisplayEl.innerText = `Displaying ${filteredEps.length}/${allEpisodes.length} Episodes`;
+
+  makePageForEpisodes(filteredEps);
+});
+
+//Level 300 (Select input)
 
 let selectEl = document.getElementById("all-episodes");
 
-selectEl.addEventListener('change', selectAnEpisode);
+selectEl.addEventListener("change", selectAnEpisode);
 
-  function selectAnEpisode(e){
-  // let optionEl = document.querySelector('option');
-  let selectEpisode = allEpisodes.filter((episode) => {
+function selectAnEpisode(e) {
+  let selectEpisode = allEpisodes.filter(episode => {
     return e.target.value.includes(episode.name);
-  }); 
-  
-  makePageForEpisodes(selectEpisode)
+  });
 
-  // if (selectEl.value === "all episodes") {
-  //   makePageForEpisodes(allEpisodes);
-  // } else {
-  //   makePageForEpisodes(selectEpisode);
-  // }
-  //clears displayNumber text when user selects any option
+  if (selectEl.value === "all episodes") {
+    makePageForEpisodes(allEpisodes);
+  } else {
+    makePageForEpisodes(selectEpisode);
+  }
   numberDisplayEl.innerText = "";
 }
